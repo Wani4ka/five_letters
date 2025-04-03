6c0677adbcabe47833b25cc32b6971f1b4a2634d
Ivan Lutsenka <ivlutsenko@ozon.ru>
2025-04-03 15:58:13 +0300

    Add basic game logic

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
+import game
+
 if __name__ == '__main__':
-    print('hello world')
+    room_id = game.create_room()
+
+    while True:
+        guess = input()
+        try:
+            response, removed = game.try_guess(room_id, guess)
+        except Exception as e:
+            print(str(e))
+        else:
+            print(response)
+            if removed:
+                break
