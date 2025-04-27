0adfceb584d76d225e4736040cdbc790db52114c
mflkee <makeevgleb86rus@gmail.com>
2025-04-27 19:26:40 +0900

    add solution
    print solution in the end

--- game/game.py
+++ game/game.py
@@ -12,12 +12,15 @@ class Game:
         self.next_room_id += 1
         return room
 
-    def try_guess(self, room_id: int, guess: str) -> (list, bool):
+    def try_guess(self, room_id: int, guess: str) -> (list, bool, str | None):
         if room_id not in self.rooms:
             raise Exception('Комната не найдена')
         room = self.rooms[room_id]
         response = room.try_guess(guess)
         finished = room.is_finished(guess)
+
+        solution = None
         if finished:
+            solution = room.word
             del self.rooms[room_id]
-        return response, finished
+        return response, finished, solution
--- main.py
+++ main.py
@@ -9,11 +9,12 @@ if __name__ == '__main__':
     while True:
         guess = input("Введите слово: ")
         try:
-            response, finished = game.try_guess(room.room_id, guess)
+            response, finished, solution = game.try_guess(room.room_id, guess)
         except Exception as e:
             print(str(e))
         else:
             print("Подсказки:", response)
             if finished:
                 print("Игра окончена!")
+                print(f"Слово было: {solution}")
                 break
